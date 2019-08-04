0007ca056c265c55a34888c58f63c76c25f8a647
hualigushi <hualigushi@163.com>
2019-08-04 22:30:19 +0800

    ‘mfirst commit

--- src/router.js
+++ src/router.js
@@ -1,23 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Ebook from './Ebook.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/ebook'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/ebook',
+      name: 'ebook',
+      component: Ebook
     }
   ]
 })
